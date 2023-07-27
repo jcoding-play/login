ba79a4891356b6323a36cbc3121bcba6c2a51774
gyungchan Jo <namejgc@naver.com>
2023-07-27 13:33:47 +0900

    feat(MemberRepository): 회원 저장 및 회원 조회 기능 구현

---
+++ src/main/java/hello/login/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package hello.login.repository;
+
+import hello.login.domain.member.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+
+    Member findById(Long id);
+
+    Optional<Member> findByLoginId(String loginId);
+
+    List<Member> findAll();
+ }
---
+++ src/main/java/hello/login/repository/MemoryMemberRepository.java
@@ -0,0 +1,42 @@
+package hello.login.repository;
+
+import hello.login.domain.member.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public void clearStore() {
+        store.clear();
+    }
+
+    @Override
+    public Member save(Member member) {
+        member.save(store, ++sequence);
+        return member;
+    }
+
+    @Override
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    @Override
+    public Optional<Member> findByLoginId(String loginId) {
+        List<Member> members = findAll();
+
+        return members.stream()
+                .filter(member -> member.isLoginIdMatch(loginId))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
