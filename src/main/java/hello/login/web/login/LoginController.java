38ac0e9f8dc31c98cdef606b323468e29d6aef5d
gyungchan Jo <namejgc@naver.com>
2023-07-28 00:03:43 +0900

    feat(LoginController): 로그인 컨트롤러 구현

---
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -0,0 +1,38 @@
+package hello.login.web.login;
+
+import hello.login.domain.member.Member;
+import hello.login.service.login.LoginService;
+import hello.login.web.login.form.LoginForm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+    private final LoginService loginService;
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute LoginForm form) {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm form) {
+        try {
+            Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+            log.info("loginMember={}", loginMember);
+            return "success";
+        } catch (IllegalArgumentException e) {
+            log.info("login fail={}", e.getMessage());
+            return "login";
+        }
+    }
+}
