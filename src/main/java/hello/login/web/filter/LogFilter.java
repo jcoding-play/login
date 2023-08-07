683e04d3bf7623a06a1ff33c10edbdf3f2737970
gyungchan Jo <namejgc@naver.com>
2023-08-07 19:03:28 +0900

    feat(LogFilter): 모든 요청을 로그로 남기는 필터 구현

---
+++ src/main/java/hello/login/web/filter/LogFilter.java
@@ -0,0 +1,39 @@
+package hello.login.web.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+public class LogFilter implements Filter {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        String uuid = UUID.randomUUID().toString();
+        try {
+            log.info("REQUEST [{}][{}]", uuid, requestURI);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+}
